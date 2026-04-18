c51fee2b08dd6e3248521aa09a0a3d9d86c18c1b
vladlenakhtemov <vladlen_ak@icloud.com>
2026-04-18 22:47:33 +0200

    refactor(build): reorganize root project plugins structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,17 @@
 plugins {
+    // --- Android ---
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    // Serialization
-    alias(libs.plugins.jetbrains.kotlin.plugin.serialization) apply false
-    // Hilt
-    alias(libs.plugins.google.dagger.hilt.android) apply false
-    // KSP
-    alias(libs.plugins.google.devtools.ksp) apply false
+
+    // --- Kotlin ---
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.jetbrains.kotlin.plugin.serialization) apply false
+
+    // --- DI ---
+    alias(libs.plugins.google.dagger.hilt.android) apply false
+
+    // --- Codegen ---
+    alias(libs.plugins.google.devtools.ksp) apply false
 }
